bba3ca01c0c1033d4810a498603c6690fddb575e
Andre Kradolfer <narfdre@gmail.com>
2012-11-04 15:55:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express   = require('express'),
+    http      = require('http'),
+    path      = require('path'),
+    rsj       = require('rsj');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.all('/*', function(req, res, next) {
+  console.log('setting headers');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/', function(req, res){
+  res.send('test');
+});
+
+app.get('/json/*', function(req, res){
+  rsj.r2j(req.params[0],function(json) {
+    console.log(json);
+    res.send(json);
+  });
+});
+
+app.get('/xml/*', function(req, res){
+  console.log(req.params[0]);
+  http.get(req.params[0], function(response) {
+    response.on('data', function (chunk) {
+      res.send(chunk);
+    });
+  }).on('error', function(e) {
+    res.send("Got error: " + e.message);
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
